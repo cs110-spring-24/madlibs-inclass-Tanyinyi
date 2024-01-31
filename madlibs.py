83834db77a74be2effd90d4c8fafc0286b1a0dbc
Tanyinyi <2295137444@qq.com>
2024-01-31 00:48:56 -0800

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,14 @@
-# TODO Create your inputs, variables, and add your story here!
+#add 10 more inputs, update your story along the way
+#hello everyone, my prounce is she/her and my name is Lydia and I have a cute cat.
+greeting=input("Enter a greeting word:")
+prouns=input("Enter your prouns:")
+name=input("Enter your name:")
+hobby=input("Enter your hobby:")
+pet=input("Enter your pet:")
+pet_name=input("Enter your pet's name:")
+pet_age=input("Enter your pet's age:")
+pet_color=input("Enter your pet's color:")
+pet_size=input("Enter your pet's size:")
+pet_favorite_food=input("Enter your pet's favorite food:")
+
+print(f"{greeting} ,my prouns are {prouns} ,my name is {name},my hobby is {hobby},I have a pet {pet},which is called {pet_name},he is {pet_age} years old now,he is {pet_color} and {pet_size},his favorite food is {pet_favorite_food}",sep=",",end=".")
